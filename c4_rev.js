aa7c1d57238288ef2bb725e54f0c08ebf785269a
wildan ismail <wildanwork86@gmail.com>
2022-05-29 19:36:05 +0700

    challenge 4

---
+++ c4_rev.js
@@ -0,0 +1,19 @@
+function indexPrime(param1) {
+  var result = [2];
+  for (let i=2; i++;) {
+      let prime=true
+    for (let j=2; j<i; j++) {
+        if( i%j==0){
+            prime=false
+        }
+    }
+    if(prime == true){
+        result.push(i)
+    }
+    if(result.length== param1){
+        break;
+    }
+  }
+  return result[param1];
+}
+console.log(indexPrime(4))
