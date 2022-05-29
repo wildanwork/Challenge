63b2f76d201ff82a6ee3cb189f001d9fd16bb264
wildan ismail <wildanwork86@gmail.com>
2022-05-29 20:00:36 +0700

    c5 done

---
+++ c5.js
@@ -0,0 +1,10 @@
+function stringManipulation(word){
+    if (word[0] == 'a' || word[0] == 'u' || word[0] == 'e' || word[0] == 'i' || word[0] == 'o'){
+    console.log(word);
+} else {
+   let a = word.slice(1)
+   console.log(a + word[0]+ 'nyo')
+}
+}
+stringManipulation('ayam')
+stringManipulation('bebek')
